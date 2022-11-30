d84ae5bfdad3fff40d3a539d83b56cd960a1a0f4
Haider <Haiderali360@gmail.com>
2022-11-30 00:35:31 -0500

    Added Cage

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,12 +11,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float _turnSpeed = 50000f;
     [SerializeField] float _forwardSpeed = 4000f;
     [SerializeField] float _reverseSpeed = 4000f;
+    [SerializeField] float _mouseSensitivity = 1f;
+    [SerializeField] int _vertLookInvert = 1;
 
+    float _xRotation = 0f;
+    float _yRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -57,5 +64,20 @@ public class PlayerController : MonoBehaviour
             }
         }*/
         
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
+        float y = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime * _vertLookInvert;
+
+        _xRotation -= y;
+        _xRotation = Mathf.Clamp(_xRotation, -50f, 40f);
+        _yRotation += x;
+        _yRotation = Mathf.Clamp(_yRotation, -55f, 55f);
+
+        Debug.Log("Mouse X: " + x + ", Mouse Y: " + y + ", xRot: " + _xRotation + ", yRot: " + _yRotation);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(_xRotation, _yRotation, 0);
     }
 }
